c9417f37621c1bc1aee768e760172da0fa4f8f5a
Fellipe796898 <uchu88965@gmail.com>
2025-04-03 11:42:44 -0400

    exercicio

--- 1.e.js
+++ 1.e.js
@@ -1,3 +1,11 @@
-console.log(nome)
-var nome = 'Notebook'
-var preço = 'Preço'
+//console.log('o notebook tal ta pelo preço tal[preço]')
+//var nome = 'Notebook'
+//var idade= '15'
+//nome.tofixed(2);
+//tipos de dados 
+//var:valor mudavel ou possivel de ser redeclarado
+//let:mesma coisa var
+//valor definido:valor imutavel
+//função temm parentes,propies não
+// lenhtn cont a quantidade de elementos,console.log(nome.length) caracteres,e tambem organiza ideias de mesmo elemento
+//undifiend,variavel não recebeu valor ou não foi declarada
---
+++ exercicios.js
@@ -0,0 +1,22 @@
+let nome = "Notebook";
+let idade = 15;
+console.log("o notebook " + nome + "esta com " + idade + "anos");
+
+//2)
+let d = 'não esta indo';
+console.log(d.toLowerCase());
+console.log(d.toUpperCase());
+
+//3)
+
+let js = 15;
+console.log(typeof js);
+
+//4)
+
+let backeruim = 1.87;
+let peso = 75;
+let imc = (peso / (backeruim ** backeruim));
+console.log = (imc.toFixed(2));
+
+console.log = ('hello word');
