6a0989d3502c02ddcc1a13d269000e3001e816aa
Fellipe796898 <uchu88965@gmail.com>
2025-09-30 08:11:17 -0400

    Create dados.js

---
+++ dados/dados.js
@@ -0,0 +1,138 @@
+const colecaoUF = [
+    {
+        id: '1',
+        uf: 'AC',
+        nome: 'ACRE'
+    },
+    {
+        id: '2',
+        uf: 'AL',
+        nome: 'ALAGOAS'
+    },
+    {
+        id: '3',
+        uf: 'AP',
+        nome: 'AMAPÁ'
+    },
+    {
+        id: '4',
+        uf: 'AM',
+        nome: 'AMAZONAS'
+    },
+    {
+        id: '5',
+        uf: 'BA',
+        nome: 'BAHIA'
+    },
+    {
+        id: '6',
+        uf: 'CE',
+        nome: 'CEARÁ'
+    },
+    {
+        id: '7',
+        uf: 'DF',
+        nome: 'DISTRITO FEDERAL'
+    },
+    {
+        id: '8',
+        uf: 'ES',
+        nome: 'ESPÍRITO SANTO'
+    },
+    {
+        id: '9',
+        uf: 'GO',
+        nome: 'GOIÁS'
+    },
+    {
+        id: '10',
+        uf: 'MA',
+        nome: 'MARANHÃO'
+    },
+    {
+        id: '11',
+        uf: 'MT',
+        nome: 'MATO GROSSO'
+    },
+    {
+        id: '12',
+        uf: 'MS',
+        nome: 'MATO GROSSO DO SUL'
+    },
+    {
+        id: '13',
+        uf: 'MG',
+        nome: 'MINAS GERAIS'
+    },
+    {
+        id: '14',
+        uf: 'PA',
+        nome: 'PARÁ'
+    },
+    {
+        id: '15',
+        uf: 'PB',
+        nome: 'PARAÍBA'
+    },
+    {
+        id: '16',
+        uf: 'PR',
+        nome: 'PARANÁ'
+    },
+    {
+        id: '17',
+        uf: 'PE',
+        nome: 'PERNAMBUCO'
+    },
+    {
+        id: '18',
+        uf: 'PI',
+        nome: 'PIAUÍ'
+    },
+    {
+        id: '19',
+        uf: 'RJ',
+        nome: 'RIO DE JANEIRO'
+    },
+    {
+        id: '20',
+        uf: 'RN',
+        nome: 'RIO GRANDE DO NORTE'
+    },
+    {
+        id: '21',
+        uf: 'RS',
+        nome: 'RIO GRANDE DO SUL'
+    },
+    {
+        id: '22',
+        uf: 'RO',
+        nome: 'RONDÔNIA'
+    },
+    {
+        id: '23',
+        uf: 'RR',
+        nome: 'RORAIMA'
+    },
+    {
+        id: '24',
+        uf: 'SC',
+        nome: 'SANTA CATARINA'
+    },
+    {
+        id: '25',
+        uf: 'SP',
+        nome: 'SÃO PAULO'
+    },
+    {
+        id: '26',
+        uf: 'SE',
+        nome: 'SERGIPE'
+    },
+    {
+        id: '27',
+        uf: 'TO',
+        nome: 'TOCANTINS'
+    }
+];
+export default colecaoUF
